b5d1aca13c0583c0981b835c3a6408073aeb569a
lala <shinbm219@naver.com>
2026-04-24 22:09:07 +0900

    AOP 적용 예제2 - AOP적용

--- src/main/java/hello/aop/exam/ExamService.java
+++ src/main/java/hello/aop/exam/ExamService.java
@@ -1,6 +1,7 @@
 package hello.aop.exam;
 
 
+import hello.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ public class ExamService {
 
     private final ExamRepository examRepository;
 
+    @Trace
     public void request(String itemId) {
         examRepository.save(itemId);
     }
--- src/test/java/hello/aop/exam/ExamServiceTest.java
+++ src/test/java/hello/aop/exam/ExamServiceTest.java
@@ -1,11 +1,13 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-
+@Import(TraceAspect.class)
 @SpringBootTest
 class ExamServiceTest {
 
